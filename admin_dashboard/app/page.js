161ad630c9c61510f00c7e43be6933104696c00c
mohammed <mohammedhanda109@gmail.com>
2023-07-17 02:35:40 +0100

    init project

---
+++ admin_dashboard/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className='bg-blue-400 w-screen h-screen'>test
+    <button className='bg-white'> login with google </button>
+    </div>
+  )
+}
