40955964bcbeddc7ecb0907df3b18a1bf07bca35
mhanda <mohammedhanda109@gmail.com>
2023-07-17 05:33:52 +0100

    add google auth

--- admin_dashboard/app/page.js
+++ admin_dashboard/app/page.js
@@ -1,8 +1,23 @@
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
+  const { data: session } = useSession()
+  
+  if (!session) {
+    return (
+      <div className="bg-blue-200 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button className='bg-white p-2 rounded-lg px-4' onClick={signIn}>Login</button>
+        </div>
+      </div>
+    );
+  }
+  
   return (
-    <div className='bg-blue-400 w-screen h-screen'>test
-    <button className='bg-white'> login with google </button>
+    <div className="bg-blue-200 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <button className='bg-white p-2 rounded-lg px-4' onClick={signOut}>Logout</button>
+      </div>
     </div>
-  )
+  );
 }
