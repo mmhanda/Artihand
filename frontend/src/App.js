ff0d757e478661978c67651ad83b732b2f5e3fc3
mohammed <mohammedhanda109@gmail.com>
2023-08-10 00:04:09 +0100

    add footer and custom bootstrap css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Header from "./components/Heard";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 
 const App = () => {
   return(
-    <div>
-    <h1>welcom to artihan</h1>
-    </div>
+    <>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <h1>welcom to artihan</h1>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   );
 };
 
