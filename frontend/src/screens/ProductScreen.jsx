c279f01fd62b71ec0d1a112f11f4435328af5c60
mohammed <mohammedhanda109@gmail.com>
2023-08-11 02:59:33 +0100

    update product card

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import products from "../productes";
 import { Link } from "react-router-dom";
-import { Col, Row, Image, ListGroup, Card, Button } from "react-bootstrap";
+import { Col, Row, Image, ListGroup, Card, Button, ListGroupItem } from "react-bootstrap";
 import Rating from "../components/Rating";
 
 const ProductScreen = () => {
@@ -10,6 +10,50 @@ const ProductScreen = () => {
   return (
     <>
     <Link className="btn btn-light my-3" to="/" >Go Back</Link>
+    <Row>
+      <Col md={5}>
+        <Image src={product.image} alt={product.name} fluid />
+      </Col>
+
+      <Col md={4}>
+      <ListGroup variant="flush">
+        <ListGroup.Item>
+          <h3> {product.name} </h3>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <h4>Price ${product.price}</h4>
+        </ListGroup.Item>
+      </ListGroup>
+
+      </Col>
+      <Col md={3}>
+      <Card>
+        <ListGroup>
+          <ListGroup.Item>
+            <Row>
+              <Col>Price:</Col>
+              <Col> <strong>{product.price}</strong></Col>
+            </Row>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Row>
+              <Col>Status:</Col>
+              <Col> <strong>{product.countInStock > 0 ? 'In stock' : "Out Of Stock"}</strong></Col>
+            </Row>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Button className="btn-block" type="button"
+                    disabled={product.countInStock === 0}>
+              Add To Cart
+            </Button>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
+      </Col>
+    </Row>
     </>
   )
 };
