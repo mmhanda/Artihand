c04eab48363ac6d9c4d7a3ca236280ec3b93cac9
mohammed <mohammedhanda109@gmail.com>
2023-08-12 02:38:12 +0100

    update the seeder

--- backend/seeder.js
+++ backend/seeder.js
@@ -10,3 +10,25 @@ import Connectdb from "./config/db.js";
 dotenv.config();
 
 Connectdb();
+
+const importData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUsers = await User.insertMany(users);
+    const userAdmin = createdUsers[0]._id;
+    
+    console.log(userAdmin);
+
+    const producteSample = products.map((product) => {
+      return { ...product, user: userAdmin };
+    })
+    
+    const createdProducts = await Product.insertMany(producteSample);
+    console.log(" Data imported sucessfully ");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
