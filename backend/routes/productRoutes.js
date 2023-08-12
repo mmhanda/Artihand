7109d8d133f9da8269536cd198a015dfb81788a1
mohammed <mohammedhanda109@gmail.com>
2023-08-12 04:19:15 +0100

    product router added

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import products from '../data/productes.js';
+
+const router = express.Router();
+
+router.get('/:id', (req, res) => {
+  const product = products.find((e) => e._id == req.params.id)
+  console.log(req.params.id);
+  if (product)
+    res.json(product);
+  else {
+    res.send("Data Not found!");
+  }
+});
+
+router.get('/', (req, res) => {
+  res.json(products);
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,19 @@
 import express from "express";
-import products from "./data/productes.js"
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.get('/:num', (req, res) => {
+app.get('/', (req, res) => {
   if (req.params.num == "pass") {
     res.send("hicsc");
   }
 });
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((e) => e._id == req.params.id)
-  res.json(product);
-})
-
-app.get('/api/products', (req, res) => {
-  res.json(products);
-})
+app.use('/api/products/', productRoutes);
 
 app.listen(process.env.PORT || 5000, () => { console.log("server is runing....") });
