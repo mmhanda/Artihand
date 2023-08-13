d93d8e2504b09b0dedb332c508eb221d7dfd7089
mohammed <mohammedhanda109@gmail.com>
2023-08-13 11:28:15 +0100

    cleaning the code a bit

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,38 +1,15 @@
 import { createSlice} from '@reduxjs/toolkit';
+import { updateCart } from '../utils/cartUtils';
 
 const initialState = localStorage.getItem('cart') ? JSON.parse(
   localStorage.getItem('cart')) : {cartItems: []}
-
-const addDecimals = (number) => {
-  return (Math.round(number * 100) / 100).toFixed(2);
-}
 
 const carteSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
     addToCart(state, action) {
-      const item = action.payload;
-      
-      const existItem = state.cartItems.find((e) => e._id === item._id)
-      if (existItem) {
-        state.cartItems = state.cartItems.map((x) => x._id === existItem._id ? item : x)
-      }
-      else {
-        state.cartItems = [...state.cartItems, item];
-      }
-
-      //calculate the total price:
-      state.itemPrice = addDecimals(state.cartItems.reduce((acc, item) => 
-        acc + item.price * item.qty, 0)); // 0 is the initial value of acc
-
-      // if the price more than 100$ shippnig will be free
-      state.shippingPrice = addDecimals(state.itemPrice > 100 ? 0 : 10);
-      state.taxPrice = addDecimals(Number(state.itemPrice * 0.15).toFixed(2));//taxes
-      state.totalPrice = Number(state.itemPrice +
-                                state.shippingPrice +
-                                state.taxPrice).toFixed(2);
-      localStorage.setItem('cart', JSON.stringify(state));
+      return updateCart(state, action);
     },
   },
 });
---
+++ frontend/src/utils/cartUtils.js
@@ -0,0 +1,30 @@
+
+export const addDecimals = (number) => {
+  return (Math.round(number * 100) / 100).toFixed(2);
+}
+
+export const updateCart = (state, action) => {
+  const item = action.payload;
+
+  const existItem = state.cartItems.find((e) => e._id === item._id)
+  if (existItem) {
+    state.cartItems = state.cartItems.map((x) => x._id === existItem._id ? item : x)
+  }
+  else {
+    state.cartItems = [...state.cartItems, item];
+  }
+
+  //calculate the total price:
+  state.itemPrice = addDecimals(state.cartItems.reduce((acc, item) => 
+    acc + item.price * item.qty, 0)); // 0 is the initial value of acc
+
+  // if the price more than 100$ shippnig will be free
+  state.shippingPrice = addDecimals(state.itemPrice > 100 ? 0 : 10);
+  state.taxPrice = addDecimals(Number(state.itemPrice * 0.15).toFixed(2));//taxes
+  state.totalPrice = Number(state.itemPrice +
+                            state.shippingPrice +
+                            state.taxPrice).toFixed(2);
+  localStorage.setItem('cart', JSON.stringify(state));
+
+  return (state);
+}
