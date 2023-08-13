b5f6971802c0105d07d295fb4c824797bf1238f7
mohammed <mohammedhanda109@gmail.com>
2023-08-13 02:16:52 +0100

    fix manifast error

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { ApiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
-  
+  reducer: { [ApiSlice.reducer]: ApiSlice.reducer },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+              .concat(ApiSlice.middleware),
+  devTools: process.env.NODE_ENV === 'development' ? true : false,
 });
 
 export default store;
