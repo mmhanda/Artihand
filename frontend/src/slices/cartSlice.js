37c5e272aca5af02f4faa6e345b2fbee0088715c
mohammed <mohammedhanda109@gmail.com>
2023-08-13 09:28:50 +0100

    adding counters to the cart reducer

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,8 +1,12 @@
 import { createSlice} from '@reduxjs/toolkit';
-import e from 'express';
+import { json } from 'react-router-dom';
 
 const initialState = localStorage.getItem('cart') ? JSON.parse(
   localStorage.getItem('cart')) : {cartItems: []}
+
+const addDecimals = (number) => {
+  return (Math.round(number * 100) / 100).toFixed(2);
+}
 
 const carteSlice = createSlice({
   name: 'cart',
@@ -18,7 +22,19 @@ const carteSlice = createSlice({
       else {
         state.cartItems = [...state.cartItems, item];
       }
-    }
+
+      //calculate the total price:
+      state.itemPrice = addDecimals(state.cartItems.reducer((acc, item) => (
+        acc + item.price * item.qty), 0)); // 0 is the initial value of acc
+      
+      // if the price more than 100$ shippnig will be free
+      state.shippingPrice = addDecimals(state.itemPrice > 100 ? 0 : 10);
+      state.taxPrice = addDecimals(Number(state.itemPrice * 0.15).toFixed(2));//taxes
+      state.totalPrice = Number(state.itemPrice +
+                                state.shippingPrice +
+                                state.taxPrice).toFixed(2);
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
   },
 });
 
