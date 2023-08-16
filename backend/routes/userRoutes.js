3f5ce521b9b7bcff56dd64ab6475e17e8b9b690f
mohammed <mohammedhanda109@gmail.com>
2023-08-16 14:43:02 +0100

    update user profile in the database working

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,7 +10,10 @@ import { isAdmin, protect } from "../middleware/authMiddleware.js";
 router.route('/').get(protect, isAdmin, getUsers).post(registerUser);
 router.post('/logout', protect, logoutUser);
 router.post('/auth', authUser);
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
-router.route("/:id", ).delete(protect, isAdmin, deleteUser).get(protect, isAdmin, getUserById).put(protect, isAdmin, updateUser);
+router.route('/profile').get(protect, getUserProfile)
+        .put(protect, updateUserProfile);
+router.route("/:id", ).delete(protect, isAdmin, deleteUser)
+        .get(protect, isAdmin, getUserById)
+          .put(protect, isAdmin, updateUser);
 
 export default router;
