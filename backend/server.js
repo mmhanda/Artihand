ec3b427277d1b809669e6fab1434114af2e90b60
mohammed <mohammedhanda109@gmail.com>
2023-08-16 05:34:19 +0100

    initial the cookieparser

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import usersRoutes from "./routes/userRoutes.js";
@@ -11,6 +12,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   if (req.params.num == "pass") {
