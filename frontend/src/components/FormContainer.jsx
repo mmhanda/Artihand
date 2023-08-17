662a19a752c56cdd6cab2d1ac9d7b159d906bcc2
mohammed <mohammedhanda109@gmail.com>
2023-08-17 11:41:15 +0100

    make formcontainer for the input and the login screen

---
+++ frontend/src/components/FormContainer.jsx
@@ -0,0 +1,15 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const FormContainer = ({children}) => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          { children }
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default FormContainer;
---
+++ frontend/src/screens/LoginSreen.jsx
@@ -0,0 +1,7 @@
+import FormContainer from "../components/FormContainer";
+
+const LoginScreen = () => {
+  
+};
+
+export default LoginScreen;
