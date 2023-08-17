ebdb5577dfc44a30f3b565c4602b586a672d2f87
mohammed <mohammedhanda109@gmail.com>
2023-08-17 06:25:04 +0100

    make user auth request slice

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,18 @@
+import { USERS_URL } from "../constants";
+import { ApiSlice } from "./apiSlice";
+
+export const usersApiSlice = ApiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({ // mutation is for the POST requeste && query is for GET
+
+      query: (data) => ({
+        url: USERS_URL/auth,
+        method: 'POST',
+        body: data,
+      }),
+
+    }),
+  }),
+});
+
+export const { useLoginMutation } = usersApiSlice;
