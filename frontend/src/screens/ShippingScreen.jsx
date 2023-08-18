5a70e29bfee5a9a50f67866a644c346cf0589b33
mohammed <mohammedhanda109@gmail.com>
2023-08-18 23:35:36 +0100

    make checkout steps header

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -5,6 +5,7 @@ import { Form, Button } from "react-bootstrap";
 import { toast } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
 import { saveShippingAddress } from '../slices/cartSlice';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 const ShippingScreen = () => {
   const cart = useSelector((state) => state.cart);
@@ -34,6 +35,7 @@ const ShippingScreen = () => {
 
   return(
     <FormContainer>
+    <CheckoutSteps step1 step2/>
       <h1> Shipping </h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='address' className='my-2'>
