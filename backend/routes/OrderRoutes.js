e6c74b4279c655199d32a76cd5fbee2fb9148ed2
mohammed <mohammedhanda109@gmail.com>
2023-08-19 03:03:31 +0100

    all routes tested with postman working

--- backend/routes/OrderRoutes.js
+++ backend/routes/OrderRoutes.js
@@ -8,8 +8,8 @@ from "../controllers/ordersController.js";
 import { isAdmin, protect } from "../middleware/authMiddleware.js";
 
 router.route('/').get(protect, isAdmin, getOrders).post(protect, addOrderItems);
-router.route('/myorders').get(protect, getMyOrders);
-router.route('/:id').get(isAdmin, protect, getOrderById);
+router.route('/mine').get(protect, getMyOrders);
+router.route('/:id').get(protect, isAdmin, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
 router.route('/:id/deliver').put(protect, isAdmin, updateOrderToDelivered);
 
