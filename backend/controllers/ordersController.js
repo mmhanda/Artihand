5776c1dd6f7b80c8c45b00ab5c239d9456d29939
mohammed <mohammedhanda109@gmail.com>
2023-08-22 00:33:09 +0100

    make the update to paid controller

--- backend/controllers/ordersController.js
+++ backend/controllers/ordersController.js
@@ -55,7 +55,28 @@ const getOrderById = asyncHandler( async(req, res) => {
 })
 
 const updateOrderToPaid = asyncHandler( async(req, res) => {
-  res.send("update Order To Paid");
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = new Date();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+    try {
+      const updateOrder = await order.save();
+      return res.status(200).json(updateOrder);
+    } catch (error) {
+      console.error(error);
+    }
+    
+  } else {
+    return res.status(404);
+    throw new Error("Order Not Found");
+  }
 });
 
 const updateOrderToDelivered = asyncHandler( async(req, res) => {
@@ -63,7 +84,15 @@ const updateOrderToDelivered = asyncHandler( async(req, res) => {
 });
 
 const getOrders = asyncHandler( async(req, res) => {
-  res.send("get All Orders");
+  const orders = Order.find();
+
+  if (orders) {
+    return res.status(200).json(orders);
+  }
+  else {
+    return res.status(404);
+    throw new Error('Orders Not Found');
+  }
 });
 
 export { addOrderItems, getMyOrders,
